0c1d0a9e332c3d0dc160e3df72cbac74e24237c1
Murad Nurmukhamedov <mns130691@gmail.com>
2020-08-26 10:02:43 +0300

    CRUD complete

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,7 +18,27 @@ Route::get('/', function () {
 });
 
 //Route::get('/insert', function (){
-//    $user = User::findOrFail(2);
-//    $address = new Address(['name'=>'21312321 Baker Street UK']);
-//    $user->address()->save($address);
-//});
+////    $user = User::findOrFail(2);
+////    $address = new Address(['name'=>'21312321 Baker Street UK']);
+////    $user->address()->save($address);
+////});
+
+Route::get('/update', function(){
+
+    $address = Address::whereUserId(1)->first();
+//    $address = Address::where('user_id', 1) ;  # equal to query bellow
+
+    $address->name = "43434 Updated Av, Moscow";
+    $address->save();
+});
+
+Route::get('/read', function(){
+    $user = User::findOrFail(1);
+    echo $user->address->name;
+});
+
+Route::get('/delete', function(){
+    $user = User::findOrFail(1);
+    $user->address()->delete();
+    return "done";
+});
