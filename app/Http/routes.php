e0aded59c9b2f2f679a13e2e4cc192aed596b845
Murad Nurmukhamedov <mns130691@gmail.com>
2020-08-26 10:56:16 +0300

    make many to many evironment

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -44,27 +44,30 @@ Route::get('/', function () {
 //    return "done";
 //});
 
-Route::get('/create', function(){
-    $user = User::findOrFail(3);
-    $posts = new Post(['title'=>'JavaScript', 'body'=>'JS seems nice to me now']);
-    $user->posts()->save($posts);
-});
+//Route::get('/create', function(){
+////    $user = User::findOrFail(3);
+////    $posts = new Post(['title'=>'JavaScript', 'body'=>'JS seems nice to me now']);
+////    $user->posts()->save($posts);
+////});
+////
+////Route::get('/read', function(){
+////    $user = User::findOrFail(1);
+////    foreach($user->posts as $post){
+////        echo $post->title . "<br>";
+////    }
+//////     dd($user);
+////});
+////
+////Route::get('/update', function(){
+////    $user = User::findOrFail(1);
+////    $user->posts()->whereId(1)->update(['title'=>'Updated Title', 'body'=>'Updated Body']);
+////    return 'success';
+////});
+////
+////Route::get('/delete', function(){
+////    $user = User::find(1);
+////    $user->posts()->whereId(1)->delete();
+////});
+//
 
-Route::get('/read', function(){
-    $user = User::findOrFail(1);
-    foreach($user->posts as $post){
-        echo $post->title . "<br>";
-    }
-//     dd($user);
-});
 
-Route::get('/update', function(){
-    $user = User::findOrFail(1);
-    $user->posts()->whereId(1)->update(['title'=>'Updated Title', 'body'=>'Updated Body']);
-    return 'success';
-});
-
-Route::get('/delete', function(){
-    $user = User::find(1);
-    $user->posts()->whereId(1)->delete();
-});
--- app/User.php
+++ app/User.php
@@ -31,4 +31,8 @@ class User extends Authenticatable
     public function posts(){
         return $this->hasMany('App\Post');
     }
+
+    public function roles(){
+        return $this->belongsToMany('App\Role');
+    }
 }
